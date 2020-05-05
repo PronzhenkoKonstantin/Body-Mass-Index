079de4630f480f4f656ce7bc9afe81b81c9ac656
PronzhenkoKonstantin <59308700+PronzhenkoKonstantin@users.noreply.github.com>
2020-05-05 16:28:12 +0300

    change files

--- BmiService.java
+++ BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public float calculate(float height,float weight) {
-        float bmi_index = weight / (height * height / 100) * 100;
-        return bmi_index;
+    public float calculate(float height, float weight) {
+        float bmi = weight / (height * height / 100) * 100;
+        return bmi;
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        float bmi_index = service.calculate(180,77);
-        System.out.println(bmi_index);
+        float bmi = service.calculate(180, 77);
+        System.out.println(bmi);
     }
 }
